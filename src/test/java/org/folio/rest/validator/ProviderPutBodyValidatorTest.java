4aad0f01beacc5508d5b301184ef7f86ac40a8b2
carolegodfrey <cgodfrey@ebsco.com>
2018-11-12 22:26:19 -0500

    Added Token validation tests

---
+++ src/test/java/org/folio/rest/validator/ProviderPutBodyValidatorTest.java
@@ -0,0 +1,42 @@
+package org.folio.rest.validator;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.folio.rest.exception.InputValidationException;
+import org.folio.rest.jaxrs.model.ProviderDataAttributes;
+import org.folio.rest.jaxrs.model.ProviderPutData;
+import org.folio.rest.jaxrs.model.ProviderPutRequest;
+import org.folio.rest.jaxrs.model.Token;
+import org.junit.Test;
+
+public class ProviderPutBodyValidatorTest {
+
+  private final ProviderPutBodyValidator validator = new ProviderPutBodyValidator();
+
+  @Test
+  public void shouldNotThrowExceptionWhenTokenIsMissing() {
+    ProviderPutRequest request = new ProviderPutRequest();
+    validator.validate(request);
+  }
+
+  @Test
+  public void shouldNotThrowExceptionWhenTokenIsValidLength() {
+    Token providerToken = new Token();
+    providerToken.setValue(RandomStringUtils.randomAlphanumeric(500));
+
+    ProviderPutRequest request = new ProviderPutRequest()
+        .withData(new ProviderPutData().withAttributes(new ProviderDataAttributes().withProviderToken(providerToken)));
+    validator.validate(request);
+  }
+  
+  @Test(expected = InputValidationException.class)
+  public void shouldThrowExceptionWhenTokenIsTooLong() {
+    Token providerToken = new Token();
+    providerToken.setValue(RandomStringUtils.randomAlphanumeric(501));
+
+    ProviderPutRequest request = new ProviderPutRequest()
+        .withData(new ProviderPutData().withAttributes(new ProviderDataAttributes().withProviderToken(providerToken)));
+    validator.validate(request);
+  }
+
+ 
+}
