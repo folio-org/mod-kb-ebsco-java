4e96222daba78da26d9a335b60095d2f27193610
sduvvuri-ebsco <sduvvuri@ebsco.com>
2018-11-15 09:21:47 -0500

    Address feedback on PR

--- src/main/java/org/folio/rmapi/model/CustomLabel.java
+++ src/main/java/org/folio/rmapi/model/CustomLabel.java
@@ -12,42 +12,34 @@ public class CustomLabel {
   @JsonProperty("displayOnPublicationFinder")
   private Boolean displayOnPublicationFinder;
   
-  @JsonProperty("id")
   public Integer getId() {
     return id;
   }
 
-  @JsonProperty("id")
   public void setId(Integer id) {
     this.id = id;
   }
 
-  @JsonProperty("displayLabel")
   public String getDisplayLabel() {
     return displayLabel;
   }
 
-  @JsonProperty("displayLabel")
   public void setDisplayLabel(String displayLabel) {
     this.displayLabel = displayLabel;
   }
 
-  @JsonProperty("displayOnFullTextFinder")
   public Boolean getDisplayOnFullTextFinder() {
     return displayOnFullTextFinder;
   }
 
-  @JsonProperty("displayOnFullTextFinder")
   public void setDisplayOnFullTextFinder(Boolean displayOnFullTextFinder) {
     this.displayOnFullTextFinder = displayOnFullTextFinder;
   }
 
-  @JsonProperty("displayOnPublicationFinder")
   public Boolean getDisplayOnPublicationFinder() {
     return displayOnPublicationFinder;
   }
 
-  @JsonProperty("displayOnPublicationFinder")
   public void setDisplayOnPublicationFinder(Boolean displayOnPublicationFinder) {
     this.displayOnPublicationFinder = displayOnPublicationFinder;
   }
