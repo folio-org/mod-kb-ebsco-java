0067b12b752a8e4ce85e8b21d8c9f566d3a4cb36
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2018-11-23 17:44:48 +0200

    log test name being executed

--- src/test/java/org/folio/rest/impl/WireMockTestBase.java
+++ src/test/java/org/folio/rest/impl/WireMockTestBase.java
@@ -1,6 +1,6 @@
 package org.folio.rest.impl;
 
-import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+import com.github.tomakehurst.wiremock.common.Slf4jNotifier;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.restassured.http.Header;
@@ -17,23 +17,42 @@ import org.folio.rest.util.RestConstants;
 import org.folio.util.TestUtil;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Base test class for tests that use wiremock and vertx http servers,
  * test that inherits this class must use VertxUnitRunner as test runner
  */
 public abstract class WireMockTestBase {
+
+  protected Logger log = LoggerFactory.getLogger(this.getClass());
+
   protected static final Header CONTENT_TYPE_HEADER = new Header(HttpConsts.CONTENT_TYPE_HEADER, HttpConsts.JSON_API_TYPE);
   protected static final String STUB_CUSTOMER_ID = "TEST_CUSTOMER_ID";
   protected static final String CONFIGURATION_STUB_FILE = "responses/configuration/get-configuration.json";
   protected static int port;
   protected static String host;
 
-  @org.junit.Rule
+  @Rule
+  public TestRule watcher = new TestWatcher() {
+
+    @Override
+    protected void starting(Description description) {
+      log.info("Running: {}.{}", description.getClassName(), description.getMethodName());
+    }
+
+  };
+
+  @Rule
   public WireMockRule userMockServer = new WireMockRule(
     WireMockConfiguration.wireMockConfig()
       .dynamicPort()
-      .notifier(new ConsoleNotifier(true)));
+      .notifier(new Slf4jNotifier(true)));
 
   @BeforeClass
   public static void setUpClass(final TestContext context) {
