71c34eb2335888a842e8acaaa629e35cb9393368
Andrii Paias <Andrii_Paias@epam.com>
2018-12-03 17:00:00 +0200

    Fix parsing of RM API errors
    In some cases RM API returns an array of errors and in some cases a single
    object. Changed ErrorUtil to correctly process the case of single object.
    Example of response with single object:
      {
        "message": "Customer not found",
        "code": 0,
        "subcode": 0
      }
    Example of response with array:
      [
        {
          "message": "Invalid APIKEY",
          "code": "1003",
          "subcode": 0
        }
      ]

--- src/main/java/org/folio/rest/util/ErrorUtil.java
+++ src/main/java/org/folio/rest/util/ErrorUtil.java
@@ -5,12 +5,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import io.vertx.core.json.DecodeException;
-import io.vertx.core.json.JsonObject;
 import org.folio.rest.jaxrs.model.JsonapiError;
 import org.folio.rest.jaxrs.model.JsonapiErrorResponse;
 import org.folio.rmapi.exception.RMAPIServiceException;
 import org.folio.rmapi.model.Errors;
+
+import io.vertx.core.json.DecodeException;
+import io.vertx.core.json.JsonObject;
 
 /**
  * Util class for creating errors
@@ -24,31 +25,38 @@ public class ErrorUtil {
   }
 
   public static JsonapiError createError(String errorMessage, String errorMessageDetails) {
-    JsonapiError configurationError = new JsonapiError();
-    JsonapiErrorResponse error = new JsonapiErrorResponse();
-    error.setTitle(errorMessage);
+    JsonapiError error = new JsonapiError();
+    JsonapiErrorResponse errorResponse = new JsonapiErrorResponse();
+    errorResponse.setTitle(errorMessage);
     if (errorMessageDetails != null) {
-      error.setDetail(errorMessageDetails);
+      errorResponse.setDetail(errorMessageDetails);
     }
-    configurationError.setErrors(Collections.singletonList(error));
-    configurationError.setJsonapi(RestConstants.JSONAPI);
-    return configurationError;
+    error.setErrors(Collections.singletonList(errorResponse));
+    error.setJsonapi(RestConstants.JSONAPI);
+    return error;
   }
-  
+
   public static JsonapiError createErrorFromRMAPIResponse(RMAPIServiceException rmApiException) {
     try {
       final JsonObject instanceJSON = new JsonObject(rmApiException.getResponseBody());
       Errors errors = instanceJSON.mapTo(Errors.class);
       JsonapiError configurationError = new JsonapiError();
-      List<JsonapiErrorResponse> jsonApiErrors = errors.getErrorList().stream()
+      Errors errorsObject = errors;
+
+      if (errorsObject.getErrorList() == null) {
+        errorsObject = errorsObject.toBuilder()
+          .errorList(Collections.singletonList(instanceJSON.mapTo(org.folio.rmapi.model.Error.class)))
+          .build();
+      }
+
+      List<JsonapiErrorResponse> jsonApiErrors = errorsObject.getErrorList().stream()
         .map(error -> new JsonapiErrorResponse()
           .withTitle(error.getMessage()))
         .collect(Collectors.toList());
       configurationError.setErrors(jsonApiErrors);
       configurationError.setJsonapi(RestConstants.JSONAPI);
       return configurationError;
-    }
-    catch(DecodeException e){
+    } catch (DecodeException e) {
       //If RM API didn't return valid json then just include response body as error message
       return createError(rmApiException.getMessage());
     }
