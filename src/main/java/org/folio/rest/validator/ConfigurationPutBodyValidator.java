520241073dce0bdb7800bb5ac8ccd1b3b6be8b80
Andrii Paias <Andrii_Paias@epam.com>
2018-12-21 18:05:50 +0200

    Make configuration attributes to be not required
    Jira: MODKBEKBJ-128
    Configuration doesn't always have all attributes present, if attribute is
    not set, then it is not returned in response

---
+++ src/main/java/org/folio/rest/validator/ConfigurationPutBodyValidator.java
@@ -0,0 +1,13 @@
+package org.folio.rest.validator;
+
+import org.folio.rest.jaxrs.model.ConfigurationAttributes;
+import org.folio.rest.jaxrs.model.ConfigurationPutRequest;
+
+public class ConfigurationPutBodyValidator {
+  public void validate(ConfigurationPutRequest request) {
+    ConfigurationAttributes attributes = request.getData().getAttributes();
+    ValidatorUtil.checkIsNotNull("API key", attributes.getApiKey());
+    ValidatorUtil.checkIsNotNull("Customer ID", attributes.getCustomerId());
+    ValidatorUtil.checkIsNotNull("API endpoint", attributes.getRmapiBaseUrl());
+  }
+}
