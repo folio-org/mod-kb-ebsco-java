f2d763d52d16a920a695fa7e84ee5e1e4ec1a5c5
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2019-02-04 11:33:28 +0200

    added code to setup test data by environment flag

--- src/main/resources/templates/db_scripts/create-table.sql
+++ src/main/resources/templates/db_scripts/create-table.sql
@@ -4,7 +4,5 @@ ADD COLUMN record_id VARCHAR (50) NOT NULL,
 ADD COLUMN record_type VARCHAR (50) NOT NULL,
 ADD COLUMN tag VARCHAR (50) NOT NULL;
 
-INSERT INTO tags (record_id, record_type, tag) VALUES
-('583-4345-762169', 'resource', 'first tag'),
-('4345', 'package', 'second tag'),
-('762169', 'title', 'third tag');
+CREATE INDEX record_id_index ON tags (record_id);
+CREATE INDEX tag_index ON tags (tag);
---
+++ src/main/resources/templates/db_scripts/test-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ${myuniversity}_${mymodule}.tags (record_id, record_type, tag) VALUES
+('583-4345-762169', 'resource', 'EBSCO'),
+('4345', 'package', 'folio'),
+('762169', 'title', 'spitfire');
