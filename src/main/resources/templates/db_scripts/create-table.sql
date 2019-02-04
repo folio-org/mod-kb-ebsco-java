327cdc48c7163f062b944fe40a185c6df43faf31
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2019-02-04 14:50:39 +0200

    added record_type column constraint

--- src/main/resources/templates/db_scripts/create-table.sql
+++ src/main/resources/templates/db_scripts/create-table.sql
@@ -1,7 +1,7 @@
 ALTER TABLE tags
 DROP COLUMN jsonb,
 ADD COLUMN record_id VARCHAR (50) NOT NULL,
-ADD COLUMN record_type VARCHAR (50) NOT NULL,
+ADD COLUMN record_type VARCHAR (10) CHECK (record_type IN ('provider', 'package', 'title', 'resource')),
 ADD COLUMN tag VARCHAR (50) NOT NULL;
 
 CREATE INDEX record_id_index ON tags (record_id);
--- src/main/resources/templates/db_scripts/test-data.sql
+++ src/main/resources/templates/db_scripts/test-data.sql
@@ -1,4 +1,5 @@
 INSERT INTO ${myuniversity}_${mymodule}.tags (record_id, record_type, tag) VALUES
-('583-4345-762169', 'resource', 'EBSCO'),
-('4345', 'package', 'folio'),
-('762169', 'title', 'spitfire');
+('53-2767121-90099', 'resource', 'EBSCO'),
+('53-2767121', 'package', 'folio'),
+('413-1988660', 'package', 'spitfire');
+('36-2728041', 'package', 'EBSCO');
