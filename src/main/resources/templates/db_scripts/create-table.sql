b42a658aea378d122796f412ed07981609f96dac
sduvvuri-ebsco <sduvvuri@ebsco.com>
2019-03-18 14:03:33 -0400

    Update create table schema

--- src/main/resources/templates/db_scripts/create-table.sql
+++ src/main/resources/templates/db_scripts/create-table.sql
@@ -1,8 +1,8 @@
 ALTER TABLE tags
-DROP COLUMN jsonb,
-ADD COLUMN record_id VARCHAR (50) NOT NULL,
-ADD COLUMN record_type VARCHAR (10) CHECK (record_type IN ('provider', 'package', 'title', 'resource')),
-ADD COLUMN tag VARCHAR (50) NOT NULL;
+DROP COLUMN IF EXISTS jsonb,
+ADD COLUMN IF NOT EXISTS record_id VARCHAR (50) NOT NULL,
+ADD COLUMN IF NOT EXISTS record_type VARCHAR (10) CHECK (record_type IN ('provider', 'package', 'title', 'resource')),
+ADD COLUMN IF NOT EXISTS tag VARCHAR (50) NOT NULL;
 
-CREATE INDEX record_id_index ON tags (record_id);
-CREATE INDEX tag_index ON tags (tag);
+CREATE INDEX IF NOT EXISTS record_id_index ON tags (record_id);
+CREATE INDEX IF NOT EXISTS tag_index ON tags (tag);
