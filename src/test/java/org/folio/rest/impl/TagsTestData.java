68d59d12dea4a5f0d63d1c540f8c55d66c0571f2
Andrii Paias <Andrii_Paias@epam.com>
2019-05-16 12:54:45 +0300

    Add unit tests for package search
    Jira: MODKBEKBJ-236
    Add tests to EholdingsProvidersImplTest
    Move common constants into separate TestData classes
    move common package set up methods to PackagesTestUtil
    Use same provider id value (19) in different test

---
+++ src/test/java/org/folio/rest/impl/TagsTestData.java
@@ -0,0 +1,8 @@
+package org.folio.rest.impl;
+
+public class TagsTestData {
+  public static final String STUB_TAG = "test tag";
+  public static final String STUB_TAG_VALUE = "tag one";
+  public static final String STUB_TAG_VALUE_2 = "tag 2";
+  public static final String STUB_TAG_VALUE_3 = "tag 3";
+}
