37b159e5ef2b5c5fb928320869bbca40723daf41
Andrii Paias <Andrii_Paias@epam.com>
2019-05-27 13:42:57 +0300

    Save holdings in batches
    Jira: MODKBEKBJ-238
    Insert list of holdings in one transaction, by sending INSERT commands
    with 200 holdings in each command.
    Move common transaction logic into DBUtil.executeInTransaction method

--- src/main/java/org/folio/rest/repository/holdings/HoldingsRepositoryImpl.java
+++ src/main/java/org/folio/rest/repository/holdings/HoldingsRepositoryImpl.java
@@ -3,13 +3,20 @@ package org.folio.rest.repository.holdings;
 import static org.folio.tag.repository.DbUtil.getTableName;
 import static org.folio.tag.repository.DbUtil.mapVertxFuture;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import com.google.common.collect.Lists;
+
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
+import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.sql.UpdateResult;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,11 +28,13 @@ import org.folio.tag.repository.DbUtil;
 public class HoldingsRepositoryImpl implements HoldingsRepository {
 
   public static final String HOLDINGS_TABLE = "holdings";
-  public static final String ID_COLUMN = "id";
-  public static final String JSONB_COLUMN = "jsonb";
-  public static final String HOLDINGS_FIELD_LIST = String.format("%s, %s", ID_COLUMN, JSONB_COLUMN);
-  public static final String INSERT_OR_UPDATE_HOLDINGS_STATEMENT = "INSERT INTO %s(" + HOLDINGS_FIELD_LIST
-      + ") VALUES (?, ?) " + "ON CONFLICT (" + ID_COLUMN + ") DO UPDATE " + "SET " + JSONB_COLUMN + " = ?;";
+  private static final String ID_COLUMN = "id";
+  private static final String JSONB_COLUMN = "jsonb";
+  private static final String HOLDINGS_FIELD_LIST = String.format("%s, %s", ID_COLUMN, JSONB_COLUMN);
+  private static final String INSERT_OR_UPDATE_HOLDINGS_STATEMENT =
+    "INSERT INTO %s(" + HOLDINGS_FIELD_LIST + ") VALUES %s" +
+      "ON CONFLICT (" + ID_COLUMN + ") DO NOTHING";
+  private static final int MAX_BATCH_SIZE = 200;
   private Vertx vertx;
 
   @Autowired
@@ -34,15 +43,41 @@ public class HoldingsRepositoryImpl implements HoldingsRepository {
   }
 
   @Override
-  public CompletableFuture<Void> saveHolding(Holding holding, String tenantId) {
-    final String query = String.format(INSERT_OR_UPDATE_HOLDINGS_STATEMENT, getTableName(tenantId, HOLDINGS_TABLE));
-    PostgresClient postgresClient = PostgresClient.getInstance(vertx, tenantId);
+  public CompletableFuture<Void> saveHolding(List<Holding> holdings, String tenantId) {
+    return DbUtil.executeInTransaction(tenantId, vertx, (postgresClient, connection) -> {
+      List<List<Holding>> batches = Lists.partition(holdings, MAX_BATCH_SIZE);
+      CompletableFuture<Void> future = CompletableFuture.completedFuture(null);
+      for (List<Holding> batch : batches) {
+        future = future.thenCompose(o ->
+          saveHoldings(batch, tenantId, connection, postgresClient));
+      }
+      return future;
+    });
+  }
 
+  private CompletableFuture<Void> saveHoldings(List<Holding> holdings, String tenantId, AsyncResult<SQLConnection> connection, PostgresClient postgresClient) {
+    String placeholders = createInsertPlaceholders(holdings);
+    JsonArray parameters = createParameters(holdings);
+    final String query = String.format(INSERT_OR_UPDATE_HOLDINGS_STATEMENT, getTableName(tenantId, HOLDINGS_TABLE), placeholders);
     Future<UpdateResult> future = Future.future();
-    final String encodedHolding = Json.encode(holding);
-    final String holdingResourceId = holding.getVendorId() + "-" + holding.getPackageId() + "-" + holding.getTitleId();
-    final JsonArray parameters = DbUtil.createInsertOrUpdateParameters(holdingResourceId, encodedHolding);
-    postgresClient.execute(query, parameters, future.completer());
+    postgresClient.execute(connection, query, parameters, future.completer());
     return mapVertxFuture(future).thenApply(result -> null);
+  }
+
+  private String getHoldingsId(Holding holding) {
+    return holding.getVendorId() + "-" + holding.getPackageId() + "-" + holding.getTitleId();
+  }
+
+  private String createInsertPlaceholders(List<Holding> holdings) {
+    return String.join(",", Collections.nCopies(holdings.size(),"(?,?)"));
+  }
+
+  private JsonArray createParameters(List<Holding> holdings) {
+    JsonArray params = new JsonArray();
+    holdings.forEach(holding -> {
+      params.add(getHoldingsId(holding));
+      params.add(Json.encode(holding));
+    });
+    return params;
   }
 }
