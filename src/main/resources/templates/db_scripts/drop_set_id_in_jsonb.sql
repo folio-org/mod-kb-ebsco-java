35cc0a7e5141e46843b67c161c2bed37df52c4b3
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2019-09-06 13:24:41 +0300

    MODKBEKBJ-312 - Update RMB to version 27.0.0
    * updated RMB to 27.0.0

---
+++ src/main/resources/templates/db_scripts/drop_set_id_in_jsonb.sql
@@ -0,0 +1,8 @@
+DROP TRIGGER IF EXISTS set_id_in_jsonb ON tags CASCADE;
+DROP TRIGGER IF EXISTS set_id_in_jsonb ON packages CASCADE;
+DROP TRIGGER IF EXISTS set_id_in_jsonb ON providers CASCADE;
+DROP TRIGGER IF EXISTS set_id_in_jsonb ON resources CASCADE;
+DROP TRIGGER IF EXISTS set_id_in_jsonb ON titles CASCADE;
+DROP TRIGGER IF EXISTS set_id_in_jsonb ON holdings CASCADE;
+DROP TRIGGER IF EXISTS set_id_in_jsonb ON retry_status CASCADE;
+DROP TRIGGER IF EXISTS set_id_in_jsonb ON holdings_status CASCADE;
