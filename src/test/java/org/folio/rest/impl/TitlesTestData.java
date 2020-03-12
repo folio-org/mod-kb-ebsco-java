0b8ce9eb89ad42eea3bb5ebdfacffdbe22ad3ee0
Pavlo Smahin <Pavlo_Smahin@epam.com>
2020-03-12 14:33:47 +0200

    MODKBEKBJ-356 Filter titles by access type

--- src/test/java/org/folio/rest/impl/TitlesTestData.java
+++ src/test/java/org/folio/rest/impl/TitlesTestData.java
@@ -12,6 +12,7 @@ public class TitlesTestData {
   public static final String STUB_CUSTOM_TITLE_NAME = "Test Title";
 
   public static final String STUB_MANAGED_TITLE_ID = "762169";
+  public static final String STUB_MANAGED_TITLE_ID_2 = "1108525";
 
   public static final String CUSTOM_TITLE_ENDPOINT = "/rm/rmaccounts/" + STUB_CUSTOMER_ID + "/titles/" + STUB_CUSTOM_TITLE_ID;
   public static final String CUSTOM_RESOURCE_ENDPOINT = "/rm/rmaccounts/" + STUB_CUSTOMER_ID + "/vendors/" + STUB_CUSTOM_VENDOR_ID + "/packages/" + STUB_CUSTOM_PACKAGE_ID + "/titles/" + STUB_CUSTOM_TITLE_ID;
