85061cbb664d529e1527d2c6ac7c1b7e5b6e69cd
Natalia Zaitseva <37537790+natali-zaitseva@users.noreply.github.com>
2020-04-23 14:11:59 +0300

    Update GET /eholdings/proxy-types to support multiple KB credentials (#253)
    * MODKBEKBJ-416 - Update GET /eholdings/proxy-types to support multiple KB credentials
    
    * modified existing proxyTypes json to include credentialsId and total results parameters.
    * added GET /eholdings/kb-credentials/<credentialsId>/proxy-types
    * added tests for GET /eholdings/kb-credentials/<credentialsId>/proxy-types
    * modified the logic for GET /eholdings/proxy-types
    * updated tests for GET /eholdings/proxy-types

--- src/main/java/org/folio/rest/impl/EholdingsProxyTypesImpl.java
+++ src/main/java/org/folio/rest/impl/EholdingsProxyTypesImpl.java
@@ -1,6 +1,9 @@
 package org.folio.rest.impl;
 
+import static io.vertx.core.Future.succeededFuture;
+
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.ws.rs.core.Response;
 
@@ -10,15 +13,20 @@ import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.folio.rest.annotations.Validate;
 import org.folio.rest.aspect.HandleValidationErrors;
-import org.folio.rest.jaxrs.model.ProxyTypes;
+import org.folio.rest.jaxrs.resource.EholdingsKbCredentialsIdProxyTypes;
 import org.folio.rest.jaxrs.resource.EholdingsProxyTypes;
-import org.folio.rest.util.template.RMAPITemplateFactory;
+import org.folio.rest.util.ErrorHandler;
+import org.folio.service.proxytypes.ProxyTypesService;
 import org.folio.spring.SpringContextUtil;
 
-public class EholdingsProxyTypesImpl implements EholdingsProxyTypes {
+public class EholdingsProxyTypesImpl implements EholdingsProxyTypes, EholdingsKbCredentialsIdProxyTypes {
+
   @Autowired
-  private RMAPITemplateFactory rmapiTemplateFactory;
+  private ProxyTypesService proxyTypesService;
+  @Autowired
+  private ErrorHandler errorHandler;
 
   public EholdingsProxyTypesImpl() {
     SpringContextUtil.autowireDependencies(this, Vertx.currentContext());
@@ -26,11 +34,31 @@ public class EholdingsProxyTypesImpl implements EholdingsProxyTypes {
 
   @Override
   @HandleValidationErrors
-  public void getEholdingsProxyTypes(Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    rmapiTemplateFactory.createTemplate(okapiHeaders, asyncResultHandler)
-      .requestAction(context ->
-        context.getHoldingsService().retrieveProxies()
-      )
-      .executeWithResult(ProxyTypes.class);
+  public void getEholdingsProxyTypes(Map<String, String> okapiHeaders,
+                                     Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+
+    proxyTypesService.findByUser(okapiHeaders)
+      .thenAccept(proxyTypes -> asyncResultHandler.handle(succeededFuture(
+        GetEholdingsProxyTypesResponse.respond200WithApplicationVndApiJson(proxyTypes))))
+      .exceptionally(handleException(asyncResultHandler));
+  }
+
+  @Override
+  @Validate
+  @HandleValidationErrors
+  public void getEholdingsKbCredentialsProxyTypesById(String id, Map<String, String> okapiHeaders,
+                                                      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+
+    proxyTypesService.findByCredentialsId(id,okapiHeaders)
+      .thenAccept(proxyTypes -> asyncResultHandler.handle(succeededFuture(
+        GetEholdingsKbCredentialsProxyTypesByIdResponse.respond200WithApplicationVndApiJson(proxyTypes))))
+      .exceptionally(handleException(asyncResultHandler));
+  }
+
+  private Function<Throwable, Void> handleException(Handler<AsyncResult<Response>> asyncResultHandler) {
+    return throwable -> {
+      errorHandler.handle(asyncResultHandler, throwable);
+      return null;
+    };
   }
 }
