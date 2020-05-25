b06e7e06415be6f254fe5059ee782606da8bd6de
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2020-05-25 13:32:37 +0300

    update scripts

--- src/main/resources/templates/db_scripts/create-packages-table.sql
+++ src/main/resources/templates/db_scripts/create-packages-table.sql
@@ -6,3 +6,7 @@ ADD COLUMN IF NOT EXISTS name VARCHAR (200) NOT NULL,
 ADD COLUMN IF NOT EXISTS content_type VARCHAR (50);
 
 CREATE INDEX IF NOT EXISTS package_name_index ON packages (name);
+
+ALTER TABLE packages
+  DROP CONSTRAINT IF EXISTS packages_pkey,
+  ADD CONSTRAINT packages_pkey PRIMARY KEY (id, credentials_id);
