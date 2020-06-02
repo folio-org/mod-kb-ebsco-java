769448532f8a5291e1448a0a39eee2fc9ed8ef09
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2020-06-02 13:17:25 +0300

    - introduce liquibase driven schema updates - move credentials/packages/providers/resources/titles changes to liquibase changelogs

--- src/main/java/org/folio/rest/impl/TenantApiImpl.java
+++ src/main/java/org/folio/rest/impl/TenantApiImpl.java
@@ -19,6 +19,7 @@ import io.vertx.core.logging.LoggerFactory;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
+import org.folio.liquibase.LiquibaseUtil;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.TenantAttributes;
 import org.folio.rest.persist.PostgresClient;
@@ -27,11 +28,13 @@ import org.folio.spring.SpringContextUtil;
 
 public class TenantApiImpl extends TenantAPI {
 
+  private static final Logger logger = LoggerFactory.getLogger(TenantApiImpl.class);
+
   private static final String TEST_DATA_SQL = "templates/db_scripts/test-data.sql";
   private static final String TENANT_PLACEHOLDER = "${myuniversity}";
   private static final String MODULE_PLACEHOLDER = "${mymodule}";
   private static final String TEST_MODE = "test.mode";
-  private final Logger logger = LoggerFactory.getLogger(TenantApiImpl.class);
+
 
   public TenantApiImpl() {
     super();
@@ -43,14 +46,34 @@ public class TenantApiImpl extends TenantAPI {
   public void postTenant(TenantAttributes entity, Map<String, String> headers, Handler<AsyncResult<Response>> handlers,
                          Context context) {
 
-    Promise<Response> promise = Promise.promise();
-    super.postTenant(entity, headers, promise, context);
+    Promise<Response> postTenant = Promise.promise();
+    super.postTenant(entity, headers, postTenant, context);
 
-    promise.future().compose(response -> setupTestData(headers, context).map(response))
+    String tenantId = TenantTool.tenantId(headers);
+    Vertx vertx = context.owner();
+
+    postTenant.future()
+      .compose(executeSchemaScripts(vertx, tenantId)::map)
+      .compose(setupTestData(vertx, tenantId)::map)
       .setHandler(handlers);
   }
 
-  private Future<List<String>> setupTestData(Map<String, String> headers, Context context) {
+  private Future<Object> executeSchemaScripts(Vertx vertx, String tenantId) {
+    Promise<Object> promise = Promise.promise();
+
+    vertx.executeBlocking(p -> {
+      logger.info("************ Running schema updates ************");
+
+      LiquibaseUtil.initializeSchemaForTenant(vertx, tenantId);
+
+      logger.info("Schema updated for tenant: {}", tenantId);
+      p.complete();
+    }, promise);
+
+    return promise.future();
+  }
+
+  private Future<List<String>> setupTestData(Vertx vertx, String tenantId) {
     try {
 
       if (!Boolean.TRUE.equals(Boolean.valueOf(System.getenv(TEST_MODE)))) {
@@ -70,13 +93,12 @@ public class TenantApiImpl extends TenantAPI {
         return Future.succeededFuture();
       }
 
-      String tenantId = TenantTool.tenantId(headers);
       String moduleName = PostgresClient.getModuleName();
 
       sqlScript = sqlScript.replace(TENANT_PLACEHOLDER, tenantId).replace(MODULE_PLACEHOLDER, moduleName);
 
       Promise<List<String>> promise = Promise.promise();
-      PostgresClient.getInstance(context.owner()).runSQLFile(sqlScript, false, promise);
+      PostgresClient.getInstance(vertx).runSQLFile(sqlScript, false, promise);
 
       logger.info("Received flag to initialize test data. Check the server log for details.");
       logger.info("************ Creating test data ************");
