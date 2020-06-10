168b443e7aa9ec963b93f29f19c8350abf0eeaf7
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2020-06-10 13:01:02 +0300

    MODKBEKBJ-444  Migration scripts with Liquibase (#278)
    * - introduce liquibase driven schema updates
    - move credentials/packages/providers/resources/titles changes to liquibase changelogs
    
    * - temporary make the code uncompilable
    
    * - move assigned users/access types/mappings to liquibase changelogs
    
    * - move holdings/status to liquibase changelogs
    
    * - move holdings status audit/retry status/transaction ids to liquibase changelogs
    
    * - fix liquibase call
    
    * - remove alter_tables_owner.sql
    
    * - set role to schema before running ddl statements
    - rename access_types_mapping if present
    
    * - ignore precondition failure
    
    * - change owner of the tables that might potentially exist before the script applied
    
    * - set deferrable="false" for constraints
    
    * - change unique constraint to pk constraint handing in assigned users repo
    - fix tests

--- src/main/resources/templates/db_scripts/create-packages-table.sql
+++ src/main/resources/templates/db_scripts/create-packages-table.sql
@@ -1,12 +1,7 @@
 ALTER TABLE packages
 DROP COLUMN IF EXISTS jsonb,
 ALTER COLUMN id TYPE VARCHAR (50),
-ADD COLUMN IF NOT EXISTS credentials_id UUID NOT NULL CONSTRAINT packages_kb_credentials_fkey REFERENCES kb_credentials,
 ADD COLUMN IF NOT EXISTS name VARCHAR (200) NOT NULL,
 ADD COLUMN IF NOT EXISTS content_type VARCHAR (50);
 
 CREATE INDEX IF NOT EXISTS package_name_index ON packages (name);
-
-ALTER TABLE packages
-  DROP CONSTRAINT IF EXISTS packages_pkey,
-  ADD CONSTRAINT packages_pkey PRIMARY KEY (id, credentials_id);
--- src/main/resources/templates/db_scripts/create-titles-table.sql
+++ src/main/resources/templates/db_scripts/create-titles-table.sql
@@ -1,11 +1,6 @@
 ALTER TABLE titles
 DROP COLUMN IF EXISTS jsonb,
 ALTER COLUMN id TYPE VARCHAR (50),
-ADD COLUMN IF NOT EXISTS credentials_id UUID NOT NULL CONSTRAINT titles_kb_credentials_fkey REFERENCES kb_credentials,
 ADD COLUMN IF NOT EXISTS name VARCHAR (200) NOT NULL;
 
 CREATE INDEX IF NOT EXISTS titles_name_index ON titles (name);
-
-ALTER TABLE titles
-  DROP CONSTRAINT IF EXISTS titles_pkey,
-  ADD CONSTRAINT titles_pkey PRIMARY KEY (id, credentials_id);
