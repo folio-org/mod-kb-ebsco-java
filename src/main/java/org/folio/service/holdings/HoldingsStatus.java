02824ef054bcfdca6045b708793af267e892c355
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2020-06-12 11:27:46 +0300

    removed unnecessary lombok @ToString

--- src/main/java/org/folio/service/holdings/HoldingsStatus.java
+++ src/main/java/org/folio/service/holdings/HoldingsStatus.java
@@ -1,13 +1,11 @@
 package org.folio.service.holdings;
 
 import lombok.Builder;
-import lombok.ToString;
 import lombok.Value;
 
 import org.folio.repository.holdings.LoadStatus;
 
 @Value
-@ToString
 @Builder(toBuilder = true)
 public class HoldingsStatus {
   String transactionId;
