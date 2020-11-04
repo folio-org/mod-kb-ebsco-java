bf2cd2c0a9e735002c1d1653613645718b0d2039
Pavlo Smahin <Pavlo_Smahin@epam.com>
2020-11-04 13:19:01 +0200

    MODKBEKBJ-517 Change custom labels' max value length

--- src/main/java/org/folio/properties/customlabels/CustomLabelsProperties.java
+++ src/main/java/org/folio/properties/customlabels/CustomLabelsProperties.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Component;
 public class CustomLabelsProperties {
 
   int labelMaxLength;
+  int valueMaxLength;
 }
--- src/main/java/org/folio/rest/validator/TitlesPostBodyValidator.java
+++ src/main/java/org/folio/rest/validator/TitlesPostBodyValidator.java
@@ -1,16 +1,17 @@
 package org.folio.rest.validator;
 
-import static org.folio.rest.validator.ValidationConstants.USER_DEFINED_FIELD_MAX_LENGTH;
+import static org.folio.rest.validator.ValidatorUtil.checkMaxLength;
 
 import java.util.List;
 import java.util.Objects;
 
 import javax.validation.ValidationException;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import org.folio.properties.customlabels.CustomLabelsProperties;
 import org.folio.rest.exception.InputValidationException;
 import org.folio.rest.jaxrs.model.TitlePostDataAttributes;
 import org.folio.rest.jaxrs.model.TitlePostIncluded;
@@ -20,16 +21,13 @@ import org.folio.rest.jaxrs.model.TitlePostRequest;
  * Verifies that post data for titles are valid
  */
 @Component
+@RequiredArgsConstructor
 public class TitlesPostBodyValidator {
 
   private static final String INVALID_POST_BODY = "Invalid request body";
 
-  private TitleCommonRequestAttributesValidator attributesValidator;
-
-  @Autowired
-  public TitlesPostBodyValidator(TitleCommonRequestAttributesValidator attributesValidator) {
-    this.attributesValidator = attributesValidator;
-  }
+  private final TitleCommonRequestAttributesValidator attributesValidator;
+  private final CustomLabelsProperties customLabelsProperties;
 
   /**
    * Provides validation for the post data attributes
@@ -42,21 +40,22 @@ public class TitlesPostBodyValidator {
       throw new InputValidationException(INVALID_POST_BODY, "");
     }
     List<TitlePostIncluded> included = entity.getIncluded();
-    if (Objects.isNull(included) ||  included.isEmpty() || Objects.isNull(included.get(0))){
+    if (Objects.isNull(included) || included.isEmpty() || Objects.isNull(included.get(0))) {
       throw new InputValidationException("Missing resource", "");
     }
     if (Objects.isNull(included.get(0).getAttributes()) ||
       Objects.isNull(included.get(0).getAttributes().getPackageId()) ||
-      StringUtils.isEmpty(included.get(0).getAttributes().getPackageId())){
+      StringUtils.isEmpty(included.get(0).getAttributes().getPackageId())) {
       throw new InputValidationException("Invalid package Id", "");
     }
 
     TitlePostDataAttributes attributes = entity.getData().getAttributes();
-    ValidatorUtil.checkMaxLength("userDefinedField1", attributes.getUserDefinedField1(), USER_DEFINED_FIELD_MAX_LENGTH);
-    ValidatorUtil.checkMaxLength("userDefinedField2", attributes.getUserDefinedField2(), USER_DEFINED_FIELD_MAX_LENGTH);
-    ValidatorUtil.checkMaxLength("userDefinedField3", attributes.getUserDefinedField3(), USER_DEFINED_FIELD_MAX_LENGTH);
-    ValidatorUtil.checkMaxLength("userDefinedField4", attributes.getUserDefinedField4(), USER_DEFINED_FIELD_MAX_LENGTH);
-    ValidatorUtil.checkMaxLength("userDefinedField5", attributes.getUserDefinedField5(), USER_DEFINED_FIELD_MAX_LENGTH);
+    int valueMaxLength = customLabelsProperties.getValueMaxLength();
+    checkMaxLength("userDefinedField1", attributes.getUserDefinedField1(), valueMaxLength);
+    checkMaxLength("userDefinedField2", attributes.getUserDefinedField2(), valueMaxLength);
+    checkMaxLength("userDefinedField3", attributes.getUserDefinedField3(), valueMaxLength);
+    checkMaxLength("userDefinedField4", attributes.getUserDefinedField4(), valueMaxLength);
+    checkMaxLength("userDefinedField5", attributes.getUserDefinedField5(), valueMaxLength);
     attributesValidator.validate(attributes);
   }
 }
