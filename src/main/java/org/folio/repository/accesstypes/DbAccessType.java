a93ba24e94e4a0a1e2178a335447df79072a65fe
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2020-11-10 13:09:37 +0200

    resolve merge issue from master

--- src/main/java/org/folio/repository/accesstypes/DbAccessType.java
+++ src/main/java/org/folio/repository/accesstypes/DbAccessType.java
@@ -1,24 +1,29 @@
 package org.folio.repository.accesstypes;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
-
-import org.folio.repository.DbMetadata;
 
 @Getter
-@SuperBuilder(toBuilder = true)
-public class DbAccessType extends DbMetadata {
+@Builder(toBuilder = true)
+public class DbAccessType {
 
   private final UUID id;
   private final UUID credentialsId;
   private final String name;
   private final String description;
   private final Integer usageNumber;
+  private final OffsetDateTime createdDate;
+  private final UUID createdByUserId;
+  private final String createdByUsername;
   private final String createdByLastName;
   private final String createdByFirstName;
   private final String createdByMiddleName;
+  private final OffsetDateTime updatedDate;
+  private final UUID updatedByUserId;
+  private final String updatedByUsername;
   private final String updatedByLastName;
   private final String updatedByFirstName;
   private final String updatedByMiddleName;
