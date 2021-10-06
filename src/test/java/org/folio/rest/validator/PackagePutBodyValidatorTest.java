6487ef22fe32c150e741ea1b769a7c41948cda5c
Shans_Kaluhin <Shans_Kaluhin@epam.com>
2021-10-06 12:06:28 +0300

    Revert "Remove useless tests"
    This reverts commit 1632ca8f54df6f6cf118000ffee86a30f8ca48ed.

--- src/test/java/org/folio/rest/validator/PackagePutBodyValidatorTest.java
+++ src/test/java/org/folio/rest/validator/PackagePutBodyValidatorTest.java
@@ -1,12 +1,17 @@
 package org.folio.rest.validator;
 
+import static org.hamcrest.Matchers.containsString;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import org.folio.rest.exception.InputValidationException;
 import org.folio.rest.impl.PackagesTestData;
 import org.folio.rest.jaxrs.model.Coverage;
 import org.folio.rest.jaxrs.model.PackagePutDataAttributes;
+import org.folio.rest.jaxrs.model.Token;
 import org.folio.rest.jaxrs.model.VisibilityData;
 
 public class PackagePutBodyValidatorTest {
@@ -26,6 +31,27 @@ public class PackagePutBodyValidatorTest {
         .withVisibilityData(new VisibilityData()
           .withIsHidden(false)
           .withReason(""))));
+  }
+
+  @Test
+  public void shouldThrowExceptionWhenPackageIsNotSelectedAndCoverageIsNotEmpty() {
+    expectedEx.expect(InputValidationException.class);
+    expectedEx.expectMessage(containsString("beginCoverage"));
+    validator.validate(PackagesTestData.getPackagePutRequest(
+      new PackagePutDataAttributes()
+        .withIsSelected(false)
+        .withCustomCoverage(new Coverage()
+          .withBeginCoverage("2000-01-01"))));
+  }
+
+  @Test
+  public void shouldThrowExceptionWhenPackageIsNotSelectedAndTokenIsNotEmpty() {
+    expectedEx.expect(InputValidationException.class);
+    expectedEx.expectMessage(containsString("value"));
+    validator.validate(PackagesTestData.getPackagePutRequest(
+      new PackagePutDataAttributes()
+        .withIsSelected(false)
+        .withPackageToken(new Token().withValue("tokenValue"))));
   }
 
   @Test
