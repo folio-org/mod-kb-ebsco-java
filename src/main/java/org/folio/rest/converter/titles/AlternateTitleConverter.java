f30fff7383508c94b1e459a9de19ba0236a3fe83
Aleh Liameshka <Aleh_Liameshka@epam.com>
2022-01-18 15:18:45 +0300

    MODKBEKBJ-612 Fixed EholdingsResourcesImplTest tests

--- src/main/java/org/folio/rest/converter/titles/AlternateTitleConverter.java
+++ src/main/java/org/folio/rest/converter/titles/AlternateTitleConverter.java
@@ -4,6 +4,7 @@ import org.folio.rest.jaxrs.model.AlternateTitle;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,7 +12,7 @@ import java.util.stream.Collectors;
 public class AlternateTitleConverter implements Converter<List<org.folio.holdingsiq.model.AlternateTitle>, List<AlternateTitle>> {
   @Override
   public List<AlternateTitle> convert(List<org.folio.holdingsiq.model.AlternateTitle> source) {
-    return source == null ? null : source.stream().map(this::convert).collect(Collectors.toList());
+    return source == null ? Collections.emptyList() : source.stream().map(this::convert).collect(Collectors.toList());
   }
 
   private AlternateTitle convert(org.folio.holdingsiq.model.AlternateTitle source) {
