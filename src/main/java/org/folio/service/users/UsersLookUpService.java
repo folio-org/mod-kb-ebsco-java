f77aded37fc43daad4b6de592a690086336abd04
Javokhir Abdullaev <101543142+DieHardJava@users.noreply.github.com>
2022-04-15 11:10:45 +0500

    MODKBEKBJ-621 Rework GET request for '/kb-credentials/{kb-credentials-id}/users' (#403)
    * preview of modkbekbj-621
    
    * fix and rework modkbekbj-621
    
    * small fixes
    
    * MODKBEKBJ-621 Refactor
    
    * fixed tests added wiremock stub mod-users in EholdingsAssignedUsersImplTest
    
    Co-authored-by: psmagin <Pavlo_Smahin@epam.com>

--- src/main/java/org/folio/service/users/UsersLookUpService.java
+++ src/main/java/org/folio/service/users/UsersLookUpService.java
@@ -2,8 +2,12 @@ package org.folio.service.users;
 
 import static org.folio.util.FutureUtils.mapVertxFuture;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-
+import java.util.stream.Collectors;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.NotFoundException;
@@ -29,6 +33,7 @@ import org.springframework.stereotype.Component;
 
 import org.folio.common.OkapiParams;
 import org.folio.okapi.common.XOkapiHeaders;
+import org.folio.util.StringUtil;
 
 /**
  * Retrieves user information from mod-users /users/{userId} endpoint.
@@ -39,7 +44,10 @@ public class UsersLookUpService {
   private static final Logger LOG = LogManager.getLogger(UsersLookUpService.class);
 
   private static final String USERS_ENDPOINT_TEMPLATE = "/users/%s";
+  private static final String USERS_ENDPOINT = "/users";
+  private static final String GROUPS_ENDPOINT = "/groups";
 
+  private static final String CQL_QUERY_PARAM = "query";
   private static final String AUTHORIZATION_FAIL_ERROR_MESSAGE = "Authorization failure";
   private static final String USER_NOT_FOUND_ERROR_MESSAGE = "User not found";
   private static final String CANNOT_GET_USER_DATA_ERROR_MESSAGE = "Cannot get user data: %s";
@@ -77,6 +85,44 @@ public class UsersLookUpService {
     }
   }
 
+  public CompletableFuture<Collection<User>> lookUpUsers(List<UUID> ids, final OkapiParams okapiParams) {
+    String idsCql = "id=(" + ids.stream().map(UUID::toString)
+      .map(StringUtil::cqlEncode).collect(Collectors.joining(" OR ")) + ")";
+    return lookUpUsersUsingCQL(okapiParams, idsCql);
+  }
+
+  public CompletableFuture<Collection<Group>> lookUpGroups(List<UUID> ids, final OkapiParams okapiParams) {
+    String idsCql = "id=(" + ids.stream().map(UUID::toString)
+      .map(StringUtil::cqlEncode).collect(Collectors.joining(" OR ")) + ")";
+    return lookUpGroupsUsingCQL(okapiParams, idsCql);
+  }
+
+  private CompletableFuture<Collection<User>> lookUpUsersUsingCQL(final OkapiParams okapiParams, String query) {
+    Promise<HttpResponse<JsonObject>> promise =
+      lookUpByCQL(okapiParams, USERS_ENDPOINT, query);
+    return mapVertxFuture(promise.future().map(HttpResponse::body).map(this::mapUserCollection));
+  }
+
+  private CompletableFuture<Collection<Group>> lookUpGroupsUsingCQL(final OkapiParams okapiParams, String query) {
+    Promise<HttpResponse<JsonObject>> promise = lookUpByCQL(okapiParams, GROUPS_ENDPOINT, query);
+    return mapVertxFuture(promise.future().map(HttpResponse::body).map(this::mapGroupCollection));
+  }
+
+  private Promise<HttpResponse<JsonObject>> lookUpByCQL(OkapiParams okapiParams, String usersEndpoint, String query) {
+    MultiMap headers = new HeadersMultiMap();
+    headers.addAll(okapiParams.getHeaders());
+    headers.add(HttpHeaders.ACCEPT, HttpHeaderValues.APPLICATION_JSON);
+
+    Promise<HttpResponse<JsonObject>> promise = Promise.promise();
+    webClient.getAbs(headers.get(XOkapiHeaders.URL) + usersEndpoint)
+      .putHeaders(headers)
+      .addQueryParam(CQL_QUERY_PARAM, StringUtil.urlEncode(query))
+      .as(BodyCodec.jsonObject())
+      .expect(ResponsePredicate.create(ResponsePredicate.SC_OK, errorConverter()))
+      .send(promise);
+    return promise;
+  }
+
   private ErrorConverter errorConverter() {
     return ErrorConverter.createFullBody(result -> {
       HttpResponse<Buffer> response = result.response();
@@ -100,13 +146,38 @@ public class UsersLookUpService {
     if (user.containsKey("username") && user.containsKey("personal")) {
       builder.id(user.getString("id"));
       builder.userName(user.getString("username"));
-
+      builder.patronGroup(user.getString("patronGroup"));
       JsonObject personalInfo = user.getJsonObject("personal");
       if (personalInfo != null) {
         builder.firstName(personalInfo.getString("firstName"));
         builder.middleName(personalInfo.getString("middleName"));
         builder.lastName(personalInfo.getString("lastName"));
       }
+    } else {
+      throw new BadRequestException(USER_INFO_IS_NOT_COMPLETE_ERROR_MESSAGE);
+    }
+    return builder.build();
+  }
+
+  private Collection<User> mapUserCollection(JsonObject userCollection) {
+    Collection<User> collection = new ArrayList<>();
+    var users = userCollection.getJsonArray("users");
+    users.stream().forEach(user -> collection.add(mapUser((JsonObject) user)));
+    return collection;
+  }
+
+  private Collection<Group> mapGroupCollection(JsonObject groupCollection) {
+    Collection<Group> collection = new ArrayList<>();
+    var groups = groupCollection.getJsonArray("usergroups");
+    groups.stream().forEach(group -> collection.add(mapGroup((JsonObject) group)));
+    return collection;
+  }
+
+  private Group mapGroup(JsonObject group) {
+    Group.GroupBuilder builder = Group.builder();
+    if (group.containsKey("group")) {
+      builder.id(group.getString("id"));
+      builder.group(group.getString("group"));
     } else {
       throw new BadRequestException(USER_INFO_IS_NOT_COMPLETE_ERROR_MESSAGE);
     }
