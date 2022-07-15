5645acee9893401579a7e04d440344ed9daa2886
psmagin <Pavlo_Smahin@epam.com>
2022-07-15 18:49:43 +0300

    MODKBEKBJ-680 Fix holdings_status migration

--- src/main/resources/templates/db_scripts/create-holdings-status-table.sql
+++ src/main/resources/templates/db_scripts/create-holdings-status-table.sql
@@ -1,8 +1,4 @@
 ALTER TABLE holdings_status
--- Add unique column with value true to ensure that only one row can be created in holdings_status
-ADD COLUMN IF NOT EXISTS lock BOOL NOT NULL UNIQUE DEFAULT TRUE CHECK (lock);
-
-ALTER TABLE holdings_status
 ADD COLUMN IF NOT EXISTS process_id UUID NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000';
 ALTER TABLE holdings_status
 ALTER COLUMN process_id DROP DEFAULT;
