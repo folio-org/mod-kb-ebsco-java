b9f379dd414e607f83f8519ad97f9bb26f71ad83
Shans Kaluhin <shans_kaluhin@epam.com>
2023-07-11 10:47:19 +0300

    Force populate Holdings

--- src/main/java/org/folio/service/holdings/DefaultLoadServiceFacade.java
+++ src/main/java/org/folio/service/holdings/DefaultLoadServiceFacade.java
@@ -40,7 +40,7 @@ public class DefaultLoadServiceFacade extends AbstractLoadServiceFacade {
 
   @Override
   protected CompletableFuture<String> populateHoldings(LoadService loadingService) {
-    return loadingService.populateHoldings().thenApply(o -> null);
+    return loadingService.populateHoldingsForce().thenApply(o -> null);
   }
 
   @Override
